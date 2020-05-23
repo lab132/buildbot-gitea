8d2daa294f37a7cbdaf5fe72c200dfd728456470
Marvin Pohl <marvin@lab132.com>
2020-05-23 11:37:06 +0200

    Bumped version to v1.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
 
 setup(name='buildbot-gitea',
       version=VERSION,
